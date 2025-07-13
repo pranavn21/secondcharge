205c41ab6d4de1e5d04919507fc79abd378549cb
Abby <125830962+abbya18@users.noreply.github.com>
2025-07-13 18:26:11 -0500

    Revert "Create a home page"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
 import { UserComponent } from './user/user.component';
-import { HomepageComponent } from './home/homepage/homepage.component';
 
 export const routes: Routes = [
-    //{path: '', redirectTo: '/', pathMatch: 'full'},
-    {path: '', component: HomepageComponent},
-    {path: 'vehicles', component: VehicleListComponent},
+    {path: '', component: VehicleListComponent},
     {path: 'user', component: UserComponent},
 ];
--- src/app/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle-list/vehicle-list.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-vehicle-list',
-  imports: [
-    RouterLink, 
-    RouterLinkActive,
-    MatButtonModule,
-    MatCardModule
-  ],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './vehicle-list.component.html',
   styleUrls: ['./vehicle-list.component.css'],
   standalone: true
